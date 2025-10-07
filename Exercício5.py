cf13565a0ac1606354915950484928d61268c805
Luís Henrique <luishenriquenunes61@gmail.com>
2025-10-07 19:59:45 -0300

    Exercício5.py

--- Exercício5.py
+++ Exercício5.py
@@ -5,7 +5,7 @@ Exercício 5
 
 2- o Cliente deverá fazer um login no site com seu e-mail.
 
-3- o Cliente deverá escolher o serviço que está procurando(Corte,Barba e Corte e barba).
+3- o Cliente deverá escolher o serviço que está procurando(Corte,Barba,Corte e barba e sobrancelha).
  
 4- o Cliente deverá agendar o dia e o horário.
 
@@ -23,127 +23,130 @@ Exercício 5
 
 11- Se algum cliente desmarcar,o horário marcado deve ser restaurado.
 
-from dataclasses import dataclass
-from datetime import datetime
-#criando uma classe de publicação.classe é um modelo que define atributos(características)e métodos(ações).
 @dataclass
-class Publicacao:
-    conteudo:str
-    descricao:str
-    autor:str
-    data_hora: datetime
-    curtidas: int = 0
-#criando uma variável lista.lista é uma variável que se pode armazenar varias informações.
-lista_publicacoes = []
-#criando uma função para a classe "Publicacao",esta função serve para criar a publicacao,(def:usado para definir funções,print:exibir a mensagem escolhida,input:serve para obter dados a partir da linha de comando)
-def criar_publicacao():
-    print("\n=== CRIAR PUBLICAÇÃO ===")
-    conteudo = input("Digite o conteúdo da publicação: ")
-    descricao = input("Digite a descrição: ")
-    autor = input("Digite o nome do autor: ")
-    data_hora = datetime.now()
+class Cliente:
+    nome:str
+    celular:str
+    email:str
     
-    nova_publicacao = Publicacao(conteudo, descricao, autor, data_hora)
-    lista_publicacoes.append(nova_publicacao)
-    print ("Publicação criada com sucesso!")
-#criando uma função para a classe "Publicacao",esta função serve para curtir a publicação,(if not:usado para verificar a ausência de um valor ou de uma situação).
-def curtir_publicacao():
-    print("\n=== CURTIR PUBLICAÇÃO ===")
-    if not lista_publicacoes:
-        print("Nenhuma publicação disponível.")
-        return
-#mostra todas as publicações disponíveis
-    visualizar_feed()
-    try:
-        indice = int(input("Digite o número da publicação para curtir: ")) -1
-        if 0 <= indice < len(lista_publicacoes):
-            lista_publicacoes[indice].curtidas += 1
-            print("Publicação curtida!")
-        else:
-            print("Publicação não encontrada.")
-    except ValueError:
-        print("Número inválido.")
-#função para exibir todas as publicações em ordem
-def visualizar_feed():
-    print("\n=== FEED ===")
-    if not lista_publicacoes:
-        print("Nenhuma publicação disponível.")
-        return
+@dataclass
+class Barbeiro:
+    nome:str
+    disponibilidade:str
+    horario:str
+    serviço:str
     
-    for i,pub in enumerate(lista_publicacoes,1):
-        print(f"(i). {pub.autor} - {pub.curtidas} curtidas")
-        print(f" {pub.conteudo[:50]}...")
-        print(f" {pub.data_hora.strftime('%d/%m/%Y %H:%M')}")
-        print("-" * 40)
- #função para visualizar uma publicação completa (individual)       
-def visualizar_publicacao_individual():
-    print("\n=== VISUALIZAR PUBLICAÇÃO ===")
-    if not lista_publicacoes:
-        print("Nenhuma publicação disponível.")
-        return
+lista_barbeiros = ["Gustavo", "Lucas", "Breno"]
+@dataclass
+class Agendamento:
+    data:str
+    hora:str
+    barbeiro:str
     
-    visualizar_feed()
-    try:
-        indice = int(input("Digite o número da publicação: ")) -1
-        if 0 <= indice < len(lista_publicacoes):
-            pub = lista_publicacoes[indice]
-            print(f"\nAutor: {pub.autor}")
-            print(f"Data: {pub.data_hora.strftime('%d/%m/%Y %H:%M')}")
-            print(f"Conteúdo: {pub.conteudo}")
-            print(f"Descrição: {pub.descricao}")
-            print(f"Curtidas: {pub.curtidas}")
-        else:
-            print("Publicação não encontrada.")
-    except ValueError:
-        print("Número inválido.")
- #função para visualizar uma publicações de um autor específico  
-def visualizar_publicacoes_por_autor():
-    print("\n=== PUBLICAÇÕES POR AUTOR ===")
-    if not lista_publicacoes:
-        print("Nenhuma publicação disponível.")
-        return
+lista_horarios = ["10h", "11h", "14h", "15h30", "16h"]
+@dataclass
+class Serviços:
+    corte:str
+    barba:str
+    corte e barba:str
+    sobrancelha:str
     
-    autor = input("Digite o nome do autor: ")
-    publicacoes_autor = [pub for pub in lista_publicacoes if pub.autor.lower() == autor.lower()]
+lista_serviços = ["corte", "barba", "sobrancelha"]
+
+@dataclass
+class Pagamentos:
+    pix:str
+    crédito:str
+    débito:str
+    dinheiro:str
+
+lista_pagamentos = ["pix", "crédito", "débito", "dinheiro"]    
+
+def agendar():
+    nome = input("Digite seu nome: ")
+    email = input("Digite seu email: ")
+    barbeiro = input("Escolha seu barbeiro: ")
+    data = input("Qual data deseja?: ")
+    hora = input("Escolha a Hora desejada: ")
+    pagamento = input("Escolha a forma de pagamento: ")
+    celular = input("Qual seu contato?: ")
+    cliente_digitado = cliente(nome,email,celular)
+    lista.append(cliente_digitado)
+    print("Cadastro realizado com sucesso")
     
-    if not publicacoes_autor:
-        print(f"Nenhuma publicação encontrada para {autor}.")
-        return
-    
-    print(f"\nPublicações de {autor}:")
-    for pub in publicacoes_autor:
-        print(f"- {pub.conteudo[:50]}...({pub.curtidas} curtidas)")
-        print(f"  {pub.data_hora.strftime('%d/%m/%Y %H:%M')}")
-        print("-" * 30)
- #função principal (menu do sistema)      
 def menu():
-    while True:
-        print("\n=== REDE SOCIAL ===")
-        print("1. Criar Publicação")
-        print("2. Curtir Publicação")
-        print("3. Visualizar Feed")
-        print("4. Visualizar Publicação Individual")
-        print("5. Visualizar Publicações por Autor")
-        print("0. Sair")
+    print("1 - Cadastrar agendamento")
+    print("2 - Ver serviços")
+    print("3 - Preço do serviço ")
+    print("4 - Ver horario disponivel")
+    print("5 - Ver clientes cadastrados")
+    print("6 - Sair")
+    return input("Escolha uma opção: ")
+#variaveis de controle
+
+
+while True:
+    opcao = menu()
         
-        opcao = input("Escolha uma opção: ")
+    if opcao == "1":
+        nome = input("Nome do cliente: ")
+        contato = input("Inserir contato: ")
+        email = input("Digite seu email: ")
+        cliente = Cliente(nome,contato,email)
+        lista_pessoas.append(cliente)
+        data = input("Inserir data: ")
+        hora = input("Digite o horário: ")
+        barbeiro = input("Escolha seu barbeiro: ")
+        servico = input("Tipo de serviço: ")
+        agendamento = Agendamento(data,hora,barbeiro)
+        pagamento = input("Escolha a forma de pagamento: ")
+        lista_serviços.append(agendamento)    
+        print(f"agendamento '{agendamento}' agendado com sucesso!")
         
-        if opcao == "1":
-            criar_publicacao()
-        elif opcao == "2":
-            curtir_publicacao()
-        elif opcao == "3":
-            visualizar_feed()
-        elif opcao == "4":
-            visualizar_publicacao_individual()
-        elif opcao == "5":
-            visualizar_publicacoes_por_autor()
-        elif opcao == "0":
-            print("Saindo...")
-            break
-        else:
-            print("Opção inválida!")
+    if opcao == "2":
+        print("\n--- Serviços ---")
+        
+        print("\n--- Cortes ---")
+        for servico in lista_cortes:
+            print(f"- {servico}")
+        
+        print("\n--- Barbas ---")
+        for servico in lista_barba:
+            print(f"- {servico}")
             
-if __name__ == "__main__":
-    menu()
+        print("\n--- Sobrancelhas ---")
+        for servico in lista_sombrancelha:
+            print(f"- {servico}")
+            
+        print("----------------------------")
         
+    if opcao == "3":
+        valor_do_corte=40
+        print(f"O valor do corte é R$ {valor_do_corte},00")
+        valor_da_barba=20
+        print(f"O valor da barba é R$ {valor_da_barba},00")
+        valor_da_sobrancelha=10
+        print(f"O valor da sobrancelha é R$ {valor_da_sobrancelha},00")
+        
+    if opcao == "4":
+        print("Horários disponíveis: ")
+        for horario in lista_horarios:
+            print(horario)
+        escolher = input("Escolha o horário: ")
+        if escolher in lista_horarios:
+            print(f"Você escolheu o horário {escolher}. Agendamento feito!")
+            
+    if opcao == "5":
+        print("\n--- Ver clientes cadastrados ---")
+        if not lista_pessoas:
+            print("Ainda não há clientes cadastrados!")
+        else:
+            for cliente in lista_pessoas:
+                print(f" {cliente}")
+                
+        print("Esses são os clientes cadastrados.")
+        
+    if opcao == "6":
+        print("Obrigado pela atenção!")
+        
+    break
